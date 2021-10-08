1918816eb9d668b34e48c4e348da9f68a6d40f01
Fahad <m.fahad@naheed.pk>
2021-10-08 10:55:28 +0500

    First

---
+++ app/src/main/java/com/example/tutorapp/fragment/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.tutorapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutorapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    ViewPager viewPager;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root=inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager=root.findViewById(R.id.viewPager);
+
+        return root;
+    }
+}
